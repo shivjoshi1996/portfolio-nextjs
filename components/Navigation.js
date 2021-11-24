26b649e124b43398e79d5f7def2db4f1a9e25d6f
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-11-24 13:41:13 -0800

    Basic nav styling, will need to rework

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,41 @@
 import { groq } from 'next-sanity';
+import Link from 'next/link';
+import styled from 'styled-components';
 import { getClient } from '../lib/sanity.server';
+
+const NavigationWrapper = styled.nav`
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+const NavigationContentWrapper = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  color: ${(props) => props.theme.colors.textPrimary};
+  width: 90%;
+  margin: 0 auto;
+  padding: 30px 0px;
+  text-transform: uppercase;
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.textPrimary};
+  }
+`;
 
 export default function Navigation({ nav }) {
   console.log(nav[0].title);
 
   console.log(nav[0].navItems);
   return (
-    <>
-      <h1>{nav[0].title}</h1>
-      <ul>
+    <NavigationWrapper>
+      <NavigationContentWrapper>
+        <p>{nav[0].title}</p>
         {nav[0].navItems.map((item) => (
-          <li key={item.text}>{item.text}</li>
+          <li key={item.text}>
+            <Link href={`${item?.navItemUrl?.linkUrl}`}>{item.text}</Link>
+          </li>
         ))}
-      </ul>
-    </>
+      </NavigationContentWrapper>
+    </NavigationWrapper>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,6 @@ import { usePreviewSubscription, urlFor, PortableText } from '../lib/sanity';
 import { getClient } from '../lib/sanity.server';
 import Navigation from '../components/Navigation';
 
-const StyledBackground = styled.div`
-  background-color: ${(props) => props.theme.colors.background};
-  min-height: 100vh;
-  color: ${(props) => props.theme.colors.textPrimary};
-  font-size: 10vw;
-`;
-
 const homeQuery = groq`
   *[_type == "homepage"]{
     title,
@@ -29,10 +22,10 @@ export default function Home(props) {
   const { data } = props;
   console.log(data);
   return (
-    <StyledBackground>
+    <>
       <Navigation nav={data.nav} />
       <h1>{data.home[0].title}</h1>
-    </StyledBackground>
+    </>
   );
 }
 
