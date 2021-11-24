eb216ed038d72f5ec6a74b2679f1d2c18f3edf4f
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-11-23 17:57:43 -0800

    Add basic functionality for project dynamic pages and data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles';
+
+const appTheme = {
+  colors: {
+    primary1: '#014E56',
+    primary2: '#F67E7E',
+    primary3: '#FFFFFF',
+    secondary1: '#79C8C7',
+    secondary2: '#2C6269',
+    secondary3: '#004047',
+    secondary4: '#012F34',
+    secondary5: '#002529',
+  },
+  font: `'Archivo', sans-serif`,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo:wght@500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ThemeProvider theme={appTheme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
