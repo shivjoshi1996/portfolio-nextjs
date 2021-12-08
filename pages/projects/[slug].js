f7731d6ccfbee273423ac84e899f8bd6f2479364
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-12-08 10:52:28 -0800

    Made changes to get project dynamic page working

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,106 +1,54 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { groq } from 'next-sanity';
-
-import { usePreviewSubscription } from '../../lib/sanity';
-import { getClient } from '../../lib/sanity.server';
+import client from '../../lib/client';
 import Navigation from '../../components/Navigation';
 
-/**
- * Helper function to return the correct version of the document
- * If we're in "preview mode" and have multiple documents, return the draft
- */
-
-function filterDataToSingleItem(data, preview) {
-  if (!Array.isArray(data)) {
-    return data;
+export default function Project({ project, nav }) {
+  console.log(nav);
+  if (nav && project) {
+    return (
+      <>
+        <Navigation nav={nav} />
+        <h1>{project?.title}</h1>
+        <p>{project?.slug?.current}</p>
+      </>
+    );
   }
-
-  if (data.length === 1) {
-    return data[0];
-  }
-
-  if (preview) {
-    return data.find((item) => item._id.startsWith(`drafts.`)) || data[0];
-  }
-
-  return data[0];
+  return <p>Loading</p>;
 }
 
-/**
- * Makes Next.js aware of all the slugs it can expect at this route
- *
- * See how we've mapped over our found slugs to add a `/` character?
- * Idea: Add these in Sanity and enforce them with validation rules :)
- * https://www.simeongriggs.dev/nextjs-sanity-slug-patterns
- */
 export async function getStaticPaths() {
-  const allSlugsQuery = groq`*[_type == "project" && defined(slug.current)][].slug.current`;
-  const pages = await getClient().fetch(allSlugsQuery);
+  const paths = await client.fetch(
+    groq`*[_type == "project" && defined(slug.current)][].slug.current`
+  );
 
   return {
-    paths: pages.map((slug) => `/projects/${slug}`),
+    paths: paths.map((slug) => ({ params: { slug } })),
     fallback: true,
   };
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  // Navigation Query
+export async function getStaticProps(context) {
+  const { slug = '' } = context.params;
+  console.log(slug);
+  const project = await client.fetch(
+    `
+  *[_type == "project" && slug.current == $slug][0]
+`,
+    { slug }
+  );
 
-  const NavQuery = groq`
-*[_type == "navigation" && id == "mainNav"]{
-  title,
-  navItems,
-}
-`;
-
-  const nav = await getClient(preview).fetch(NavQuery);
-
-  // Page Query
-  const query = groq`*[_type == "project" && slug.current == $slug][0]`;
-
-  const queryParams = { slug: params.slug };
-  const data = await getClient(preview).fetch(query, queryParams);
-
-  // Escape hatch, if our query failed to return data
-  if (!data) return { notFound: true };
-  if (!nav) return { notFound: true };
-
-  // Helper function to reduce all returned documents down to just one
-  const page = filterDataToSingleItem(data, preview);
-
+  const nav = await client.fetch(
+    `*[_type == "navigation" && id == "mainNav"]{
+      title,
+      navItems,
+    }`
+  );
   return {
     props: {
-      // Pass down the "preview mode" boolean to the client-side
-      preview,
-      // Pass down the initial content, and our query
-      data: { nav, page, query, queryParams },
+      project,
+      nav,
     },
   };
-}
-
-export default function Page({ data, preview }) {
-  const { data: previewData } = usePreviewSubscription(data?.query, {
-    params: data?.queryParams ?? {},
-    // The hook will return this on first render
-    // This is why it's important to fetch *draft* content server-side!
-    initialData: data?.page,
-    // The passed-down preview context determines whether this function does anything
-    enabled: preview,
-  });
-
-  // Client-side uses the same query, so we may need to filter it down again
-  const page = filterDataToSingleItem(previewData, preview);
-
-  // Notice the optional?.chaining conditionals wrapping every piece of content?
-  // This is extremely important as you can't ever rely on a single field
-  // of data existing when Editors are creating new documents.
-  // It'll be completely blank when they start!
-  return (
-    <>
-      <div style={{ maxWidth: `20rem`, padding: `1rem` }}>
-        {page?.title && <h1>{page.title}</h1>}
-      </div>
-    </>
-  );
 }
