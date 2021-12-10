9c1677610b8f5172304ca04859f9c906eb42cbb5
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-12-10 12:32:32 -0800

    WIP dynamic project page, data fetching complete

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,16 +1,113 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { groq } from 'next-sanity';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { BiArrowBack } from 'react-icons/bi';
+import BlockContent from '@sanity/block-content-to-react';
 import client from '../../lib/client';
 import Navigation from '../../components/Navigation';
+import { urlFor } from '../../lib/sanity';
+
+const ProjectSection = styled.section`
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+const StyledViewAllProjectsLink = styled.div`
+  padding: 1rem 0rem 1rem 5%;
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.textPrimary};
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+  }
+`;
+
+const ProjectHeroWrapper = styled.div`
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const StyledProjectBodyContent = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const ProjectInfoWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const StyledProjectDate = styled.div``;
+
+const StyledProjectTechnology = styled.div``;
+
+const StyledProjectRoles = styled.div``;
 
 export default function Project({ project, nav }) {
+  console.log(project);
   if (nav && project) {
     return (
       <>
         <Navigation nav={nav} />
-        <h1>{project?.title}</h1>
-        <p>{project?.slug?.current}</p>
+        <ProjectSection>
+          <StyledViewAllProjectsLink>
+            <Link className="all-projects-link" href="/projects" passHref>
+              <a>
+                <BiArrowBack />
+                View All Projects
+              </a>
+            </Link>
+          </StyledViewAllProjectsLink>
+          <ProjectHeroWrapper>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={urlFor(project.mainImage).url()}
+              alt={`${project.title} project`}
+            />
+            <h2>{project?.title}</h2>
+          </ProjectHeroWrapper>
+
+          <ProjectInfoWrapper>
+            {project?.publishedAt && (
+              <StyledProjectDate>
+                <h3>DATE</h3>
+                <p>{project?.publishedAt}</p>
+              </StyledProjectDate>
+            )}
+
+            {project?.technologies && (
+              <StyledProjectTechnology>
+                <h3>TECHNOLOGIES USED</h3>
+                {project?.technologies.map((technology) => (
+                  <p key={technology.title}>{technology.title}</p>
+                ))}
+              </StyledProjectTechnology>
+            )}
+
+            {project?.projectRoles && (
+              <StyledProjectRoles>
+                <h3>ROLES</h3>
+
+                {project?.projectRoles.map((role) => (
+                  <p key={role.title}>{role.title}</p>
+                ))}
+              </StyledProjectRoles>
+            )}
+          </ProjectInfoWrapper>
+
+          <StyledProjectBodyContent>
+            <BlockContent blocks={project.body} {...client.config()} />
+          </StyledProjectBodyContent>
+        </ProjectSection>
       </>
     );
   }
@@ -32,7 +129,15 @@ export async function getStaticProps(context) {
   const { slug = '' } = context.params;
   const project = await client.fetch(
     `
-  *[_type == "project" && slug.current == $slug][0]
+  *[_type == "project" && slug.current == $slug][0]{
+    title,
+   mainImage,
+   slug,
+   publishedAt,
+   body,
+   projectRoles[]->{title},
+   technologies[]->{title},
+  }
 `,
     { slug }
   );
