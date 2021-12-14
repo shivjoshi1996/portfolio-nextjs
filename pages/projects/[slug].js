aa27f7dbe22b0f5b57cdebf137722ec4431385f1
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-12-14 10:52:29 -0800

    Styling changes for tablet dynamic project page

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -20,7 +20,10 @@ const ProjectSection = styled.section`
 `;
 
 const StyledViewAllProjectsLink = styled.div`
-  padding: 1rem 0rem 1rem 5%;
+  padding: 2rem 0rem;
+  width: 90%;
+  margin: 0 auto;
+  max-width: 69.375rem;
 
   a {
     text-decoration: none;
@@ -32,14 +35,67 @@ const StyledViewAllProjectsLink = styled.div`
 `;
 
 const ProjectHeroWrapper = styled.div`
+  @media (min-width: 48rem) {
+    width: 90%;
+    margin: 0 auto;
+    max-width: 69.375rem;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: 'info image';
+    margin-bottom: 3rem;
+  }
   img {
     width: 100%;
     height: auto;
     margin-bottom: 0.5rem;
+
+    @media (min-width: 48rem) {
+      grid-area: image;
+      margin-bottom: unset;
+    }
+  }
+`;
+
+const StyledProjectHeroInfo = styled.div`
+  @media (min-width: 48rem) {
+    grid-area: info;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
   }
   h2 {
     margin-bottom: 1rem;
     padding: 0 0 0 5%;
+
+    @media (min-width: 48rem) { 
+      padding: unset;
+      margin-bottom: unset;
+  }
+`;
+
+const StyledProjectLinks = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  gap: 1rem;
+
+  @media (min-width: 48rem) {
+    width: 100%;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.textPrimary};
+    padding: 0.8rem;
+    border: 1px solid ${(props) => props.theme.colors.textPrimary};
+    margin-bottom: 2rem;
+
+    @media (min-width: 48rem) {
+      margin-bottom: unset;
+    }
   }
 `;
 
@@ -69,6 +125,11 @@ const ProjectInfoWrapper = styled.div`
   grid-template-columns: 1fr 1fr;
   margin-bottom: 1rem;
 
+  @media (min-width: 48rem) {
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-bottom: 3rem;
+  }
+
   h3 {
     margin-bottom: 0.5rem;
   }
@@ -76,26 +137,29 @@ const ProjectInfoWrapper = styled.div`
 
 const StyledProjectDate = styled.div``;
 
-const StyledProjectTechnology = styled.div`
-  grid-column: 1/3;
-`;
-
 const StyledProjectRoles = styled.div`
   text-align: right;
+
+  @media (min-width: 48rem) {
+    text-align: left;
+  }
 `;
 
-const StyledProjectLinks = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  gap: 1rem;
+const StyledProjectTechnology = styled.div`
+  grid-column: 1/3;
 
-  a {
-    text-decoration: none;
-    color: ${(props) => props.theme.colors.textPrimary};
-    padding: 0.8rem;
-    border: 1px solid ${(props) => props.theme.colors.textPrimary};
-    margin-bottom: 2rem;
+  @media (min-width: 48rem) {
+    grid-column: unset;
+    text-align: right;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    @media (min-width: 48rem) {
+      flex-direction: column;
+    }
   }
 `;
 
@@ -119,28 +183,30 @@ export default function Project({ project, nav }) {
               src={urlFor(project.mainImage).url()}
               alt={`${project.title} project`}
             />
-            <h2>{project?.title}</h2>
+            <StyledProjectHeroInfo>
+              <h2>{project?.title}</h2>
 
-            <StyledProjectLinks>
-              {project?.liveSiteUrl && (
-                <a
-                  href={project.liveSiteUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Live Site
-                </a>
-              )}
-              {project?.sourceCodeUrl && (
-                <a
-                  href={project.sourceCodeUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Code
-                </a>
-              )}
-            </StyledProjectLinks>
+              <StyledProjectLinks>
+                {project?.liveSiteUrl && (
+                  <a
+                    href={project.liveSiteUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View Live Site
+                  </a>
+                )}
+                {project?.sourceCodeUrl && (
+                  <a
+                    href={project.sourceCodeUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View Code
+                  </a>
+                )}
+              </StyledProjectLinks>
+            </StyledProjectHeroInfo>
           </ProjectHeroWrapper>
 
           <ProjectInfoWrapper>
@@ -155,18 +221,26 @@ export default function Project({ project, nav }) {
               <StyledProjectRoles>
                 <h3>ROLES</h3>
 
-                {project?.projectRoles.map((role) => (
-                  <p key={role.title}>{role.title}</p>
-                ))}
+                <ul>
+                  {project?.projectRoles.map((role) => (
+                    <li key={role.title}>
+                      <p>{role.title}</p>
+                    </li>
+                  ))}
+                </ul>
               </StyledProjectRoles>
             )}
 
             {project?.technologies && (
               <StyledProjectTechnology>
                 <h3>STACK</h3>
-                {project?.technologies.map((technology) => (
-                  <p key={technology.title}>{technology.title}</p>
-                ))}
+                <ul>
+                  {project?.technologies.map((technology) => (
+                    <li key={technology.title}>
+                      <p>{technology.title}</p>
+                    </li>
+                  ))}
+                </ul>
               </StyledProjectTechnology>
             )}
           </ProjectInfoWrapper>
